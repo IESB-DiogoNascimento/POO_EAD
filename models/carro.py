42e0ab26885efc91287eb49d9bea90498598f564
Diogo364 <diogotnascimento94@gmail.com>
2022-03-09 20:29:15 -0300

    Implement ID for car

--- models/carro.py
+++ models/carro.py
@@ -4,24 +4,28 @@ from dataclasses import dataclass
 @dataclass
 class Carro:
 
+    __idx: str
     categoria: str
     transmissao: str
     combustivel: str
     marca: str
     modelo: str
 
+    def get_id(self):
+        return self.__idx
+
     def set_atributo(self, nome: str, valor: str):
         setattr(self, nome, valor)
 
     @classmethod
-    def criar_carro(cls, categoria, transmissao, combustivel, marca, modelo, **kwargs):
-        novo_carro = Carro(categoria, transmissao, combustivel, marca, modelo)
+    def criar_carro(cls, idx, categoria, transmissao, combustivel, marca, modelo, **kwargs):
+        novo_carro = Carro(idx, categoria, transmissao, combustivel, marca, modelo)
         for atributo, valor in kwargs.items():
             setattr(novo_carro, atributo, valor)
         return novo_carro
     
-    def copy(self):
-        return self.criar_carro(self.categoria, self.transmissao, self.combustivel, self.marca, self.modelo)
+    def copy(self, novo_idx: int):
+        return self.criar_carro(novo_idx, self.categoria, self.transmissao, self.combustivel, self.marca, self.modelo)
 
     def __str__(self):
-        return '\n'.join([f'{attr}: {value}' for attr, value in self.__dict__.items()])
+        return '\n'.join([f'{attr}: {value}' for attr, value in self.__dict__.items() if '__idx' not in attr])
